a19c217aac8759920853f990d2eeae6a6b7dacdd
CAYUELA Annick <annickcayuela.ac@gmail.com>
2025-06-17 16:54:47 +0200

    feat(main) express route welcome

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,6 +9,14 @@ import "../database/checkConnection";
 // Import the Express application from ./app
 import app from "./app";
 
+// Message welcome
+
+import type { RequestHandler } from "express";
+
+const welcome: RequestHandler = (req, res) => {
+  res.send("Welcome to the wild series!");
+};
+
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
 
